95bb471d4442611d5b6db28f7ff1f9f4f83e3b2a
Tsussu Peng <gordon.pon@gmail.com>
2020-06-07 10:35:51 +0800

    ADD README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Restaurant List
+使用[Node.js](https://nodejs.org/en/)、[Express](https://expressjs.com/)、[mongodb](https://www.mongodb.com/) 打造餐廳清單網站
+
+
+## Requirement
+[Node.js](https://nodejs.org/en/)   
+
+## Packages 套件
+此專案用到以下 Node.js library，可藉由 `npm install` 指令去安裝   
+[express](https://expressjs.com/)   
+[express-handlebars](https://www.npmjs.com/package/express-handlebars)   
+
+***
+
+## Features 功能
++ 首頁查看餐廳資料（名稱、分類、評分）
++ 在首頁上方的搜尋方框，輸入名稱搜尋餐廳
++ 點選首頁任一餐廳圖片，可查看更詳細的資料（類別、地址、電話、評分）
--- app.js
+++ app.js
@@ -1,33 +1,35 @@
+//需要安裝的套件： node, nodemon, express, express-handlebars
+//版控工具： git
+
 const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantsList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
-
 app.get('/', (req, res) => {
-    res.render('index', {restaurants: restaurantsList.results})
+    res.render('index', {restaurants: restaurantList.results})
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-    console.log('request', req)
-    const restaurant = restaurantsList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+app.get("/restaurants/:restaurant_id", (req, res) => {
+    console.log('req.params: ', req.params.restaurant_id)
+    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
     res.render('show', {restaurant: restaurant})
 })
 
-
 app.get('/search', (req, res) => {
-    console.log('req.query.keyword ', req.query.keyword)
+    console.log('req.query', req.query.keyword)
     const keyword = req.query.keyword
-    const restaurants = restaurantsList.results.filter(restaurant => {
+    const restaurants = restaurantList.results.filter(restaurant => {
         return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
     })
     res.render('index', {restaurants: restaurants, keyword: keyword})
 })
+
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}`)
